73492104981a40dc7e2f1ecd4ac4494c2f27736f
pcchencode <zxp791228@gmail.com>
2023-12-10 14:58:46 -0800

    modify the script

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from pyspark.sql.types import IntegerType
 
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
-
+# Reading
 print("reading data from hdfs")
 s = time.time()
 df = spark.read.csv("hdfs:///usr/spotify/dataset.csv", header=True, escape='"')
@@ -16,6 +16,8 @@ print(f"It takes {exec_time}s to read data from hdfs")
 print("print out the schema")
 df.printSchema()
 
+# Analyzing
+## 1. average popularity within track_genre
 print("Calculating average popularity within track_genre")
 df = df.withColumn('popularity', df['popularity'].cast(IntegerType()))
 s = time.time()
@@ -25,21 +27,19 @@ stat_df.show()
 exec_time = e-s
 print(f"It takes {exec_time}s to calculate average popularity within track_genre")
 
-print("exporting popularity statistics data")
-stat_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/mean_populaarity.csv")
-
+# Filtering
 print("filtering data")
+s = time.time()
 low_df = df.filter(df.popularity<=40)
 med_df = df.filter((df.popularity>40) & (df.popularity<=80))
 high_df = df.filter(df.popularity>80)
-low_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/low.csv")
-med_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/med.csv")
-high_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/high.csv")
+e = time.time()
+exec_time = e-s
+print(f"It takes {exec_time}s to filter the data")
+# low_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/low.csv")
+# med_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/med.csv")
+# high_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/high.csv")
 
-
-# print("reading data from hive")
-# s = time.time()
-# spark.sql("select * from dataset").show(5)
-# e = time.time()
-# exec_time = e-s
-# print(f"It takes {exec_time}s to read data from hive")
+# Exporting
+print("exporting popularity statistics data")
+stat_df.repartition(1).write.format("com.databricks.spark.csv").mode('overwrite').option("header", "true").save("/usr/spotify/mean_populaarity.csv")
--- script_hive.py
+++ script_hive.py
@@ -4,6 +4,7 @@ from pyspark.sql.types import IntegerType
 
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
+# Reading
 print("reading data from Hive")
 s = time.time()
 df = spark.sql("SELECT * FROM dataset")
@@ -15,6 +16,7 @@ print(f"It takes {exec_time}s to read data from hive")
 print("print out the schema")
 df.printSchema()
 
+# Analyzing
 print("Calculating average popularity within track_genre")
 sql = """
 SELECT track_genre, avg(popularity) as mean_popularity
@@ -28,13 +30,20 @@ stat_df.show()
 exec_time = e-s
 print(f"Hive takes {exec_time}s to calculate average popularity within track_genre")
 
-print("exporting popularity statistics data to Hive")
-stat_df.write.mode('overwrite').saveAsTable("stat")
-
+# Filtering
 print("filtering data by Hive sql")
+s = time.time()
 low_df = spark.sql("SELECT * FROM dataset WHERE popularity<=40")
 med_df = spark.sql("SELECT * FROM dataset WHERE popularity>40 and popularity<=80")
 high_df = spark.sql("SELECT * FROM dataset WHERE popularity>80")
-low_df.write.mode('overwrite').saveAsTable("low_df")
-med_df.write.mode('overwrite').saveAsTable("med_df")
-high_df.write.mode('overwrite').saveAsTable("high_df")
+e = time.time()
+exec_time = e-s
+print(f"It takes {exec_time}s to filter the data")
+# low_df.write.mode('overwrite').saveAsTable("low_df")
+# med_df.write.mode('overwrite').saveAsTable("med_df")
+# high_df.write.mode('overwrite').saveAsTable("high_df")
+
+# Exporting
+print("exporting popularity statistics data to Hive")
+stat_df.write.mode('overwrite').saveAsTable("stat")
+
