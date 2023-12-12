49fb311213dad27339ac5b34f91bda3908358e91
pcchencode <zxp791228@gmail.com>
2023-12-12 10:43:22 -0800

    add analysis 2 into script

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import time
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import col, desc, stddev, avg, sum, count
 
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
@@ -16,7 +17,7 @@ print(f"It takes {exec_time}s to read data from hdfs")
 print("print out the schema")
 df.printSchema()
 
-# Analyzing
+# Analyzing - 1 Start
 ## 1. average popularity within track_genre
 print("Calculating average popularity within track_genre")
 df = df.withColumn('popularity', df['popularity'].cast(IntegerType()))
@@ -26,6 +27,126 @@ e = time.time()
 stat_df.show()
 exec_time = e-s
 print(f"It takes {exec_time}s to calculate average popularity within track_genre")
+
+## 2. duration vs popularity to show the relationship between the duration of songs and popularity
+print("Calculating duration vs popularity")
+s = time.time()
+# shows the duration_ms in relation to popularity of a song and displays it
+duration_popularity_df = df.select('duration_ms', 'popularity')
+duration_popularity_df.show()
+# converts duration from milliseconds to seconds
+duration_popularity_df = duration_popularity_df.withColumn('duration_sec', col('duration_ms') / 1000)
+# calculates the mean popularity for different durations and displays results
+duration_popularity_df = duration_popularity_df.groupBy('duration_sec').mean('popularity')
+duration_popularity_df.show()
+e = time.time()
+exec_time = e - s
+print(f"It takes {exec_time}s to caluclate duration vs popularity")
+
+##3. key and mode in relation with popularity
+print("Analyzing Key and Mode vs. Popularity")
+s = time.time()
+# groups the data by key and mode and calculating the mean popularity for each group.
+key_mode_popularity_df = df.groupBy('key', 'mode').mean('popularity')
+key_mode_popularity_df.show()
+
+e = time.time()
+exec_time = e - s
+print(f"It takes {exec_time}s to analyze Key and Mode vs. Popularity")
+
+##4. acousticness and instrumentalness in relation with popularity
+print("Analyzing Acousticness and Instrumentalness vs. Popularity")
+s = time.time()
+# groups the data by acousticness and instrumentalness and calculates the mean of the popularity for each group.
+acousticness_instrumentalness_popularity_df = df.groupBy('acousticness', 'instrumentalness').agg({'popularity': 'mean'})
+acousticness_instrumentalness_popularity_df.show()
+
+e = time.time()
+exec_time = e-  s
+print(f"It takes {exec_time}s to analyze Acousticness and Instrumentalness vs. Popularity")
+# Analyzing - 1 End
+
+
+# Analysis - 2 Start
+# 1. Calculating the avg tempo for each genre
+start_time = time.time()
+tempo_genre_df = df.select('tempo', 'track_genre')
+# tempo_genre_df = df.groupBy('genre').agg({'tempo': 'mean'})
+tempo_genre_df = df.groupBy('track_genre').agg(avg("tempo").alias("avg_tempo"))
+tempo_genre_df.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze Acousticness and Instrumentalness vs. Popularity")
+
+# 2. Calculate average popularity of genre when not taking songs in 4x4 in consideration
+
+start_time = time.time()
+filtered_df = df.filter(col("time_signature") != 4)
+genre_avg_popularity = (
+    filtered_df
+    .groupBy("track_genre")
+    .agg({"popularity": "avg"})
+    .withColumnRenamed("avg(popularity)", "avg_popularity")
+)
+genre_avg_popularity.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze average popularity of genre when not taking songs in 4x4 in consideration")
+
+# 3. Calculate average popularity of genre when only taking songs in 4x4 in consideration
+
+start_time = time.time()
+filtered_df = df.filter(col("time_signature") == 4)
+genre_avg_popularity = (
+    filtered_df
+    .groupBy("track_genre")
+    .agg({"popularity": "avg"})
+    .withColumnRenamed("avg(popularity)", "avg_popularity")
+)
+genre_avg_popularity.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze average popularity of genre when only taking songs in 4x4 in consideration")
+
+# 4. Compare Average Speechiness, Instrumentalness and the ratio Speechiness/Instrumentalness per genre
+
+start_time = time.time()
+
+# average speechiness per genre
+avg_speechiness_per_genre = (
+    df.groupBy("track_genre")
+    .agg({"speechiness": "avg"})
+    .withColumnRenamed("avg(speechiness)", "avg_speechiness")
+)
+
+# average instrumentalness per genre
+avg_instrumentalness_per_genre = (
+    df.groupBy("track_genre")
+    .agg({"instrumentalness": "avg"})
+    .withColumnRenamed("avg(instrumentalness)", "avg_instrumentalness")
+)
+
+# joining the two dataframes
+avg_speech_instr = (
+    avg_speechiness_per_genre
+    .join(avg_instrumentalness_per_genre, "track_genre")
+)
+
+# avg speechiness / instrumentalness per genre
+avg_speech_instr = avg_speech_instr.withColumn(
+    "avg_speech_instr_ratio",
+    avg_speech_instr.avg_speechiness / avg_speech_instr.avg_instrumentalness
+)
+avg_speech_instr.show()
+
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze Speechiness, Instrumentalness per genre")
+# Analysis - 2 End
 
 # Filtering
 print("filtering data")
--- script_hive.py
+++ script_hive.py
@@ -16,7 +16,8 @@ print(f"It takes {exec_time}s to read data from hive")
 print("print out the schema")
 df.printSchema()
 
-# Analyzing
+# Analyzing - 1 Start
+# 1 calculating averages of popularity within track genre
 print("Calculating average popularity within track_genre")
 sql = """
 SELECT track_genre, avg(popularity) as mean_popularity
@@ -29,6 +30,152 @@ e = time.time()
 stat_df.show()
 exec_time = e-s
 print(f"Hive takes {exec_time}s to calculate average popularity within track_genre")
+
+# 2 Duration vs. Popularity Analysis
+print("Analyzing duration vs. popularity")
+s = time.time()
+
+# Select duration_ms and popularity columns
+sql_duration = """
+SELECT duration_ms, popularity
+FROM dataset
+"""
+duration_df = spark.sql(sql_duration)
+
+# Convert duration_ms to seconds
+sql_duration_convert = """
+SELECT duration_ms, popularity, duration_ms / 1000 AS duration_sec
+FROM dataset
+"""
+duration_popularity_df = spark.sql(sql_duration_convert)
+
+# Group by duration_sec and calculate mean popularity
+sql_duration_group = """
+SELECT duration_sec, AVG(popularity) AS mean_popularity
+FROM (
+    SELECT duration_ms, popularity, duration_ms / 1000 AS duration_sec
+    FROM dataset
+) subquery
+GROUP BY duration_sec
+"""
+duration_df = spark.sql(sql_duration_group)
+duration_df.show()
+
+e = time.time()
+exec_time = e - s
+print(f"Hive takes {exec_time}s to analyze duration vs. popularity")
+
+# 4. Key and Mode Analysis
+print("Analyzing Key and Mode vs. Popularity")
+s = time.time()
+
+# Selects key, mode and average popularity from dataset and  groups them to compare relations
+sql_key_mode = """
+SELECT key, mode, AVG(popularity) AS mean_popularity
+FROM dataset
+GROUP BY key, mode
+"""
+key_mode_df = spark.sql(sql_key_mode)
+key_mode_df.show()
+
+e = time.time()
+exec_time = e - s
+print(f"Hive takes {exec_time}s to analyze Key and Mode vs. Popularity")
+
+# 5. Valence Analysis
+print("Analyzing Valence vs. Popularity")
+s = time.time()
+# Selects valence and average popularity from dataset and  groups them to compare relations
+sql_valence = """
+SELECT valence, AVG(popularity) AS mean_popularity
+FROM dataset
+GROUP BY valence
+"""
+valence_df = spark.sql(sql_valence)
+valence_df.show()
+
+e = time.time()
+exec_time = e - s
+print(f"Hive takes {exec_time}s to analyze valence in relation to popularity")
+# Analysis - 1 End
+
+
+
+# Analysis - 2 Start
+# 1. Calculating the avg tempo for each genre
+start_time = time.time()
+
+avg_tempo_per_genre = """
+SELECT track_genre, AVG(tempo) AS avg_tempo
+FROM dataset
+GROUP BY track_genre;
+"""
+avg_tempo_per_genre = spark.sql(avg_tempo_per_genre)
+avg_tempo_per_genre.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze Acousticness and Instrumentalness vs. Popularity")
+
+# 2. Calculate average popularity of genre when not taking songs in 4x4 in consideration
+
+start_time = time.time()
+
+avg_popul_time_sig_per_genre = """
+SELECT track_genre, AVG(popularity) AS avg_popularity
+FROM dataset
+WHERE time_signature <> 4
+GROUP BY track_genre;
+"""
+
+avg_popul_time_sig_per_genre = spark.sql(avg_popul_time_sig_per_genre)
+avg_popul_time_sig_per_genre.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze average popularity of genre when not taking songs in 4x4 in consideration")
+
+# 3. Calculate average popularity of genre when only taking songs in 4x4 in consideration
+
+start_time = time.time()
+
+avg_popul_time_sig_per_genre = """
+SELECT track_genre, AVG(popularity) AS avg_popularity
+FROM dataset
+WHERE time_signature = 4
+GROUP BY track_genre;
+"""
+avg_popul_time_sig_per_genre = spark.sql(avg_popul_time_sig_per_genre)
+avg_popul_time_sig_per_genre.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze average popularity of genre when only taking songs in 4x4 in consideration")
+
+# 4. Compare Average Speechiness, Instrumentalness and the ratio Speechiness/Instrumentalness per genre
+
+start_time = time.time()
+
+speech_instr_per_genre = """
+SELECT 
+    track_genre,
+    AVG(speechiness) AS avg_speechiness,
+    AVG(instrumentalness) AS avg_instrumentalness,
+    AVG(speechiness) / AVG(instrumentalness) AS avg_speech_instr_ratio
+FROM 
+    dataset
+GROUP BY 
+    track_genre;
+"""
+speech_instr_per_genre = spark.sql(speech_instr_per_genre)
+speech_instr_per_genre.show()
+
+end_time = time.time()
+exec_time = end_time -  start_time
+print(f"It takes {exec_time}s to analyze Speechiness, Instrumentalness per genre")
+# Analysis - 2 End
+
+
 
 # Filtering
 print("filtering data by Hive sql")
@@ -46,4 +193,3 @@ print(f"It takes {exec_time}s to filter the data")
 # Exporting
 print("exporting popularity statistics data to Hive")
 stat_df.write.mode('overwrite').saveAsTable("stat")
-
